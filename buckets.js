eb38934a6adcd4a8aae1456dc388ec86f4362f19
Jason Kraus <zbyte64@gmail.com>
2014-10-06 20:27:28 -0700

    wrapped module in UMD loader

--- buckets.js
+++ buckets.js
@@ -9,8 +9,16 @@
  * Top level namespace for Buckets, a JavaScript data structure library.
  * @namespace buckets
  */
-var buckets = {};
-(function() {
+
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define([], factory);
+    } else if (typeof exports === "object") {
+        module.exports = factory();
+    } else {
+        root.Requester = factory();
+    }
+}(this, function() {
     'use strict';
 
     /**
@@ -2366,4 +2374,4 @@ var buckets = {};
     if (typeof module !== 'undefined') {
         module.exports = buckets;
     }
-}());
+}));

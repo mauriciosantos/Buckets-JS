ea34a505593888392b3de812d73edb2a5c854a3c
Mauricio Santos <mauricio.santos@globant.com>
2015-03-12 01:11:30 -0500

    Fix typo

--- buckets.js
+++ buckets.js
@@ -1,4 +1,4 @@
-// Created by Mauricio Santos on 2013.
+// Created by Mauricio Santos in 2013.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.

49404abd09996ed64564bf291bdb2b6e10467b38
Mauricio Santos <mauriciosantoss@gmail.com>
2015-12-14 19:50:40 -0500

    Fix pushTo repository

--- gruntfile.js
+++ gruntfile.js
@@ -70,7 +70,7 @@ module.exports = function(grunt) {
 		      tagName: 'v%VERSION%',
 		      tagMessage: 'Version %VERSION%',
 		      push: true,
-		      pushTo: 'upstream',
+		      pushTo: 'origin',
 		      gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
 		      globalReplace: false,
 		      prereleaseName: false,
